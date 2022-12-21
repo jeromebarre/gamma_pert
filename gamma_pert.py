054c43707f8ce908c27eb3d9a54a6acf14ecc675
barre <barre@ucar.edu>
2022-12-21 16:32:10 -0500

    add close

--- gamma_pert.py
+++ gamma_pert.py
@@ -184,6 +184,7 @@ def main():
       
       outfile = outpath + filename.split('.')[0]+"_"+f'{imember+1:03d}'+"."+filename.split('.')[1]
       dsemi.to_netcdf(path=outfile,mode='w') 
+      dsemi.close()
 
 if __name__ == "__main__":
     main()
